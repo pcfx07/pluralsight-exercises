9fd4d97a9093b90ae8ed9e17435a9b7ba7165e33
lawly <m-plankl@web.de>
2018-04-29 17:01:15 +0200

    action results

--- aspdotnet-core-fundamentals/OdeToFood/OdeToFood/Controllers/HomeController.cs
+++ aspdotnet-core-fundamentals/OdeToFood/OdeToFood/Controllers/HomeController.cs
@@ -1,14 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using OdeToFood.Models;
 
 namespace OdeToFood.Controllers
 {
-    public class HomeController
+    public class HomeController : Controller
     {
         // ENTRY POINT by convention
-        public string Index()
+        public IActionResult Index()
         {
             // will be displayed in browser
-            return "Hello from the HomeController";
+            //return "Hello from the HomeController";
+
+            // CORE CONCEPT: seperate deciding what to do, from actual doing
+            // action determines to return a content
+            // later mvc takes that content result and turns it into http response
+            //return Content("Hello from the HomeController");
+
+            var model = new Restaurant
+            {
+                Id = 1,
+                Name = "Scott's Pizza Palace"
+            };
+
+            // something else later in the pipeline will figure out
+            // what to do with this result
+            return new ObjectResult(model);
         }
     }
 }
