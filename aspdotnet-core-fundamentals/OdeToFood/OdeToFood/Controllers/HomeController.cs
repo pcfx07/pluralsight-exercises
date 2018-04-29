1c8f95a032e82eb64c6c3d164de88838c7c2556b
lawly <m-plankl@web.de>
2018-04-29 17:18:26 +0200

    rendering views

--- aspdotnet-core-fundamentals/OdeToFood/OdeToFood/Controllers/HomeController.cs
+++ aspdotnet-core-fundamentals/OdeToFood/OdeToFood/Controllers/HomeController.cs
@@ -11,20 +11,25 @@ namespace OdeToFood.Controllers
             // will be displayed in browser
             //return "Hello from the HomeController";
 
-            // CORE CONCEPT: seperate deciding what to do, from actual doing
-            // action determines to return a content
-            // later mvc takes that content result and turns it into http response
-            //return Content("Hello from the HomeController");
-
             var model = new Restaurant
             {
                 Id = 1,
                 Name = "Scott's Pizza Palace"
             };
 
+            #region Action Results
+            // CORE CONCEPT: seperate deciding what to do, from actual doing
+            // action determines to return a content
+            // later mvc takes that content result and turns it into http response
+            //return Content("Hello from the HomeController");
+
             // something else later in the pipeline will figure out
             // what to do with this result
-            return new ObjectResult(model);
+            //return new ObjectResult(model);
+            #endregion
+
+            // RENDER MODEL AS HTML
+            return View(model);
         }
     }
 }
