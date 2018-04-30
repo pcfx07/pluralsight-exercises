234661e5c0c55c8e9ec181b3502ea0fdf7d02580
lawly <m-plankl@hotmail.com>
2018-04-30 22:08:54 +0200

    implemented db context

---
+++ aspdotnet-core-fundamentals/OdeToFood/OdeToFood/Services/SqlRestaurantData.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using OdeToFood.Data;
+using OdeToFood.Models;
+
+namespace OdeToFood.Services
+{
+    public class SqlRestaurantData : IRestaurantData
+    {
+        private OdeToFoodDbContext _context;
+
+        public SqlRestaurantData(OdeToFoodDbContext context)
+        {
+            _context = context;
+        }
+
+        // ID PROPERTY WILL BECOME AN IDENTITY COLUMN
+        // WHICH CAN BE AUTO-GENERATED!
+        public Restaurant Add(Restaurant restaurant)
+        {
+            _context.Restaurants.Add(restaurant);
+            _context.SaveChanges();
+            return restaurant;
+        }
+
+        public Restaurant Get(int id)
+        {
+            return _context.Restaurants.FirstOrDefault(x => x.Id == id);
+        }
+
+        // FOR REALLY LARGE RESULTS BETTER USE IQUERYABLE
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return _context.Restaurants.OrderBy(r => r.Name);
+        }
+    }
+}
